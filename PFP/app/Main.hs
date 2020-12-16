ae906f2ee9c7f9bc8e03ca704fc8b383787730b9
xiaomaobao <Xzhang1994@gmail.com>
2020-12-16 16:29:08 +0100

    die object

--- PFP/app/Main.hs
+++ PFP/app/Main.hs
@@ -4,3 +4,13 @@ import Lib
 
 main :: IO ()
 main = someFunc
+
+
+
+-- Probability distribution
+newtype Prob a = Prob {runProb :: [(a, Rational)]}
+
+-- die object
+die' :: Prob Integer
+die' = Prob [(x, 1/6) | x <- [1..6]]
+
